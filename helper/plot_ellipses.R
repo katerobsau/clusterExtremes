bdf7ee43c0dfebdefae9acf488f48f95c2862d4d
Kate Saunders <saundersk1@student.unimelb.edu.au>
2018-09-03 14:12:24 +1000

    editted files before merge

--- helper/plot_ellipses.R
+++ helper/plot_ellipses.R
@@ -1,12 +1,13 @@
 # plot the ellipses
- plot_ellipses <- function(base_plot, all_ellipses_df, alpha = 0.5){
+ plot_ellipses <- function(base_plot, all_ellipses_df, alpha = 0.5, plot_medoids = TRUE){
 
   ell_plot <-
     base_plot +
     geom_path(data = all_ellipses_df,
             aes(x=x, y= y, group = plot_group),
-            alpha = alpha) +
-    geom_point(data = medoids,
+            alpha = alpha)
+  if(plot_medoids)
+    ell_plot + geom_point(data = medoids,
              aes(x=x, y =y))
 
   return(ell_plot)
